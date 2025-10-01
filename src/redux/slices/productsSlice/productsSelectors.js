ed556b38b27f485fc121063a307c26a64a859863
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2025-10-01 21:27:39 +0300

    fix: use redux for producs fetch, use sceleton for preload

--- src/redux/slices/productsSlice/productsSelectors.js
+++ src/redux/slices/productsSlice/productsSelectors.js
@@ -1,13 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectProducts = (state) => state.products.items;
 export const selectProductsLoading = (state) => state.products.loading;
 export const selectProductsError = (state) => state.products.error;
 export const selectSpecials = (state) => state.products.specialItems;
 export const selectSpecialsStatus = (state) => state.products.specialStatus;
 
-export const selectProductsByCategory = (state, catId) =>
-	state.products.byCategory[catId]?.products || [];
 export const selectIsLoadingByCategory = (state, catId) =>
-	state.products.byCategory[catId]?.status === "loading";
+  state.products.byCategory[catId]?.status === "loading";
 export const selectErrorByCategory = (state, catId) =>
-	state.products.byCategory[catId]?.error;
+  state.products.byCategory[catId]?.error;
 
+const selectCategoryState = (state, catId) => state.products.byCategory[catId];
+export const selectProductsByCategory = createSelector(
+  [selectCategoryState],
+  (category) => category?.products || []
+);
